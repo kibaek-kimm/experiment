f46468fcb339520a41a459b767422ead53d4c449
Kibaek Kim <kibaek.kimm@gmail.com>
2023-03-11 17:22:01 +0900

    Add getter, setter

--- src/class/Dot.js
+++ src/class/Dot.js
@@ -1,7 +1,35 @@
 export default class Dot{
+  #x;
+  #y;
+  #size;
+
   constructor(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
+  }
+
+  set x(x) {
+    this.#x = x;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  set y(y) {
+    this.#y = y;
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  set size(size) {
+    this.#size = size;
+  }
+
+  get size() {
+    return this.#size;
   }
 }
